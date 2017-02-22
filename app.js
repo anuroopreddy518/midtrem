5691cfb8a516b6ba994e1eb707fa94dca73e69d6
anuroop <anuroop.reddy>
2017-02-22 12:31:12 -0500

    pointed to mlab

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect(config.mongodb);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var players =require('./routes/players');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/players',players);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- config/globalVars.js
+++ config/globalVars.js
@@ -2,4 +2,4 @@
 
 module.exports = {
   mongodb: 'mongodb://anuroop:pass2017@ds023064.mlab.com:23064/comp2068'
-}
+};
